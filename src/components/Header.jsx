7d3dc68b3a2856797e6e54d8045193f97708c74f
jorge2101719 <rusosky01@gmail.com>
2023-08-05 16:05:53 -0400

    pequeñas correcciones en los comentarios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 // Creación, definición; uso de props vía destructuring (punto 2 del desafío)
-
+// Se agrega un texto por defecto en caso de no aparecer otro vía app.jsx
 const Header = ({title = 'Título por defecto'}) => {
     return (
         <>

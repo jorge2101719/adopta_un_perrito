9f229ff043e0c30e044232465139d999e6a5977c
jorge2101719 <rusosky01@gmail.com>
2023-09-19 00:52:50 -0300

    nuevas acciones para hacer deploy en github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "https://github.com/jorge2101719/adopta_un_perrito",
+  plugins: [react()],
 })
